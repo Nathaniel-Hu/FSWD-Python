59e8567bb863cdd887b38585e810520687da5b25
hun4 <hun4@mcmaster.ca>
2022-05-22 21:00:15 -0400

    added top level description of library file

--- Backend/BackendManagement.py
+++ Backend/BackendManagement.py
@@ -1,3 +1,11 @@
+# ======================================================================
+# Backend Management Functions
+# ----------------------------------------------------------------------
+# These functions provide the auxiliary methods to manage the frontend
+# to backend interactions of the web application. It will be used in the
+# Python Flask web application (hosted on repl.it with db being used).
+# ======================================================================
+
 # returns value of submit button pressed; used to identify which submit
 # button was pressed to trigger a specific web application response
 def submit_pressed(form_info):
